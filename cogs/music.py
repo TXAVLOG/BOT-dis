77572d50347779a38bcad5fdcef10ea5e4fcc19d
TXAVLOG <viptretrauc@gmail.com>
2026-01-30 01:54:30 +0700

    CS

--- cogs/music.py
+++ cogs/music.py
@@ -441,6 +441,11 @@ class Music(commands.Cog):
             "http_headers": {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
                 "Referer": "https://www.youtube.com/",
+            },
+            "extractor_args": {
+                "youtube": {
+                    "player_client": ["android", "ios"]
+                }
             }
         }
         
@@ -500,6 +505,11 @@ class Music(commands.Cog):
             "http_headers": {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
                 "Referer": "https://www.youtube.com/",
+            },
+            "extractor_args": {
+                "youtube": {
+                    "player_client": ["android", "ios"]
+                }
             }
         }
         
